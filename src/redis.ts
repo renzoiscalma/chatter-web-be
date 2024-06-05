9e1b9a26f4a3e0130294106d809b901691cfb498
Renzo Calma <xeganthy@gmail.com>
2024-06-05 18:57:04 -0300

    Updated to new redis

--- src/redis.ts
+++ src/redis.ts
@@ -4,19 +4,17 @@ import Redis, { RedisOptions } from "ioredis";
 export let pubsub: RedisPubSub;
 
 const startRedisServer = async () => {
-  const { REDIS_DOMAIN, REDIS_PORT, REDIS_USER, REDIS_PW } = process.env;
-
+  const { REDIS_URI } = process.env;
+  if (!REDIS_URI) {
+    throw new Error("REDIS_URI is not set");
+  }
   const options: RedisOptions = {
     retryStrategy: (times) => Math.min(times * 50, 2000),
-    username: REDIS_USER,
-    password: REDIS_PW,
-    port: Number(REDIS_PORT),
-    host: String(REDIS_DOMAIN),
   };
 
   pubsub = new RedisPubSub({
-    publisher: new Redis(options),
-    subscriber: new Redis(options),
+    publisher: new Redis(REDIS_URI),
+    subscriber: new Redis(REDIS_URI),
   });
 };
 
